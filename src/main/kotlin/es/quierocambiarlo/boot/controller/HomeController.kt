3ffeb8f36df430ec1de19503b33e35243e749e31
Jimmy K. Oak <adrian.robles.maiz@gmail.com>
2020-12-29 13:53:16 +0100

    add moda

--- src/main/kotlin/es/quierocambiarlo/boot/controller/HomeController.kt
+++ src/main/kotlin/es/quierocambiarlo/boot/controller/HomeController.kt
@@ -1,10 +1,19 @@
 package es.quierocambiarlo.boot.controller
 
+import es.quierocambiarlo.boot.view.model.MenuCategory
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
-class HomeController {
+class HomeController(
+    private val menuCategories: List<MenuCategory>
+) {
     @GetMapping("/")
-    suspend fun home(): String = "home/index"
+    suspend fun home(model: Model): String {
+        model["menuCategories"] = menuCategories
+
+        return "home/index"
+    }
 }
