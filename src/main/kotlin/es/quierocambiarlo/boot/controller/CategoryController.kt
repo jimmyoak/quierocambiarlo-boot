a73c23d7ba1521b325b0678d73ff21630a62fc04
Jimmy K. Oak <adrian.robles.maiz@gmail.com>
2020-12-29 13:30:41 +0100

    404 when category slug not found

--- src/main/kotlin/es/quierocambiarlo/boot/controller/CategoryController.kt
+++ src/main/kotlin/es/quierocambiarlo/boot/controller/CategoryController.kt
@@ -2,11 +2,13 @@ package es.quierocambiarlo.boot.controller
 
 import es.quierocambiarlo.boot.view.model.MenuCategory
 import es.quierocambiarlo.boot.view.model.Seo
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.ResponseStatus
 
 @Controller
 class CategoryController(
@@ -15,7 +17,7 @@ class CategoryController(
 ) {
     @GetMapping("/trueques-de-{slug:[\\w-]+}.html")
     fun landing(@PathVariable slug: String, model: Model): String {
-        val seo = categoriesSeo[slug] ?: return "forward:notfoundpage"
+        val seo = categoriesSeo[slug] ?: throw CategorySlugNotFoundException()
 
         model["seo"] = seo
         model["openGraph"] = seo.toOpenGraph()
@@ -28,3 +30,6 @@ class CategoryController(
         if (it.slug == slug) it.activate() else it
     }
 }
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+class CategorySlugNotFoundException : RuntimeException()
