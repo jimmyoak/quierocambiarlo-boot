3a0dd74f6ad291bbe68cfe12fbf69030d8c49714
Jimmy K. Oak <adrian.robles.maiz@gmail.com>
2020-12-30 15:21:23 +0100

    make extension urlgenerator

--- src/main/kotlin/es/quierocambiarlo/boot/pebble/function/UrlFunction.kt
+++ src/main/kotlin/es/quierocambiarlo/boot/pebble/function/UrlFunction.kt
@@ -3,11 +3,10 @@ package es.quierocambiarlo.boot.pebble.function
 import com.mitchellbosecke.pebble.extension.Function
 import com.mitchellbosecke.pebble.template.EvaluationContext
 import com.mitchellbosecke.pebble.template.PebbleTemplate
+import es.quierocambiarlo.boot.url.DomainUrlGenerator
 
 object UrlFunction : Function {
-    private const val BASE_PATH = "https://www.quierocambiarlo.es"
     private const val PATH_ARG = "path"
-
     private val ARGUMENT_NAMES = listOf(PATH_ARG)
 
     override fun getArgumentNames() = ARGUMENT_NAMES
@@ -17,9 +16,7 @@ object UrlFunction : Function {
         self: PebbleTemplate,
         context: EvaluationContext,
         lineNumber: Int
-    ): Any {
-        return BASE_PATH + path(args)
-    }
+    ) = DomainUrlGenerator.full(path(args))
 
     private fun path(args: Map<String, Any>): String = (args[PATH_ARG] as String?) ?: "/"
 }
