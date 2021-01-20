1ee72f205060fcd203850735d59cc6f11530923b
Jimmy K. Oak <adrian.robles.maiz@gmail.com>
2021-01-20 22:43:46 +0100

    make new ad publishment view

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,12 +26,16 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
+	implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
 	implementation("io.pebbletemplates:pebble-spring-boot-starter:3.1.4")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+
+	runtimeOnly("io.r2dbc:r2dbc-postgresql")
+	runtimeOnly("org.postgresql:postgresql")
 
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 
