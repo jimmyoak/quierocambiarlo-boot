fb87a5fb40af2a682b113b9963a0b91ecd208b09
Jimmy K. Oak <adrian.robles.maiz@gmail.com>
2021-01-31 23:21:50 +0100

    add breadcrumb

--- src/main/kotlin/es/quierocambiarlo/boot/controller/web/AdController.kt
+++ src/main/kotlin/es/quierocambiarlo/boot/controller/web/AdController.kt
@@ -5,6 +5,8 @@ import es.quierocambiarlo.boot.domain.ad.orNotFound
 import es.quierocambiarlo.boot.domain.user.UserRepository
 import es.quierocambiarlo.boot.domain.user.orNotFound
 import es.quierocambiarlo.boot.infrastructure.logger.logger
+import es.quierocambiarlo.boot.view.model.MenuCategory
+import es.quierocambiarlo.boot.view.model.Seo
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.ui.set
@@ -17,7 +19,8 @@ private val LOGGER = logger<AdController>()
 @Controller
 class AdController(
     private val adRepository: AdRepository,
-    private val userRepository: UserRepository
+    private val userRepository: UserRepository,
+    private val menuCategories: List<MenuCategory>
 ) {
     @GetMapping("/cambio-{slug:[\\w-]+}/{adId:[\\w-]+}")
     suspend fun page(
@@ -29,10 +32,12 @@ class AdController(
         val ad = adRepository.findById(adId).orNotFound().let{ ad ->
             ad.copy(pictures = ad.pictures.map { it.copy(path = "/adp/${it.path}") })
         }
+        val category = menuCategories.firstOrNull { it.id == ad.categoryId.name } ?: throw CategorySlugNotFoundException()
         val user = userRepository.findById(ad.advertiserId).orNotFound()
 
         model["ad"] = ad
         model["user"] = user
+        model["category"] = category
 
         return "ad/page/index"
     }
